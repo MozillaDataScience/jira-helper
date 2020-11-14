af9d14193ac1bb71e16d89598553e52f3a072bc2
Tim D. Smith <tdsmith@mozilla.com>
2020-11-13 18:15:19 -0800

    Add some tickets models

--- src/tickets/models.py
+++ src/tickets/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class JiraTicket(models.Model):
+    summary = models.CharField(max_length=8192)
+    issue_key = models.CharField(max_length=16, primary_key=True)
+    resolution = models.CharField(max_length=16)
+    assignee = models.EmailField(max_length=255)
+    reporter = models.EmailField(max_length=255)
+    created = models.DateTimeField()
+    resolved = models.DateTimeField()
+    description = models.TextField()
+    last_comment = models.TextField()
+
+
+class Annotation(models.Model):
+    jira_ticket = models.ForeignKey(JiraTicket, on_delete=models.CASCADE)
+    abstract = models.TextField()
+    deliverable = models.URLField(max_length=8192)
