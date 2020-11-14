8f765caaeb765a7c3c28e9e0f2dfd7b21d65e9f4
Tim D. Smith <tdsmith@mozilla.com>
2020-11-14 12:34:56 -0800

    Fix slow rendering, add creature comforts

--- src/tickets/views.py
+++ src/tickets/views.py
@@ -1,32 +1,31 @@
 from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
+from django.urls import reverse
 
 from .models import Annotation, JiraTicket
 from .forms import AnnotationForm
 
 
-def _annotated(ticket):
-    try:
-        ticket.annotation
-        return True
-    except Annotation.DoesNotExist:
-        return False
-
-
 def index(request):
     tickets = JiraTicket.objects.all()
+    annotations = Annotation.objects.all()
+    annotated_ids = {a.jira_ticket_id for a in annotations}
     assignees = sorted({ticket.assignee for ticket in tickets})
     tickets_by_assignee = {}
     for a in assignees:
         completed = sorted(
-            (t for t in tickets if t.assignee == a and _annotated(t)),
+            (t for t in tickets if t.assignee == a and t.issue_key in annotated_ids),
             key=lambda t: t.created,
         )
         todo = sorted(
-            (t for t in tickets if t.assignee == a and not _annotated(t)),
+            (
+                t
+                for t in tickets
+                if t.assignee == a and t.issue_key not in annotated_ids
+            ),
             key=lambda t: t.created,
         )
-        score = "{:.0f}%".format(len(completed) / len(todo + completed) * 100)
+        score = "{:.0f}".format(len(completed) / len(todo + completed) * 100)
         tickets_by_assignee[a or "Unassigned"] = {
             "completed": completed,
             "todo": todo,
@@ -46,14 +45,42 @@ def detail(request, issue_key):
     except Annotation.DoesNotExist:
         pass
 
+    next_ticket = None
+    try:
+        next_ticket = JiraTicket.objects.filter(
+            assignee=ticket.assignee,
+            created__gt=ticket.created,
+            annotation__isnull=True,
+        ).order_by("created")[0]
+    except IndexError:
+        pass
+
     if request.method == "POST":
         form = AnnotationForm(request.POST, instance=annotation)
+        destination = request.path
+        if "save_and_next" in request.POST and next_ticket:
+            destination = reverse("detail", args=(next_ticket.issue_key,))
         if not form.is_valid():
-            return HttpResponseRedirect(request.path)
+            return HttpResponseRedirect(request.path + "?error=true")
+        if not form.has_changed():
+            return HttpResponseRedirect(destination)
         annotation = form.save(commit=False)
         annotation.jira_ticket = ticket
         annotation.save()
-        return HttpResponseRedirect(request.path)
+        return HttpResponseRedirect(destination + "?success=true")
+
+    success = "success" in request.GET
+    error = "error" in request.GET
 
     form = AnnotationForm(instance=annotation)
-    return render(request, "tickets/detail.html", {"ticket": ticket, "form": form})
+    return render(
+        request,
+        "tickets/detail.html",
+        {
+            "ticket": ticket,
+            "form": form,
+            "error": error,
+            "success": success,
+            "next_ticket": next_ticket,
+        },
+    )
